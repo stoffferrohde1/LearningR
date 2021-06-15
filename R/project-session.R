97143de68d8d93de7b8431f0a9e986cafdb88623
Christopher Rohde <stofferrohde1@gmail.com>
2021-06-15 15:15:23 +0200

    Start of project

---
+++ R/project-session.R
@@ -0,0 +1,52 @@
+source(here::here("R/package-loading.R"))
+
+# Basics of R -------------------------------------------------------------
+# LAver ny  fil: usethis::use_r("package-loading")
+
+weight_kilos <-100
+weight_kilos
+
+c("a", "b", "c")
+c(TRUE, FALSE)
+c(1, 2, 4)
+factor(c("high", "medium", "low"))
+
+* præsentere først linjer
+head(CO2, 10)
+* navne på variabler
+colnames(CO2)
+str(CO2)
+summary(CO2)
+
+
+
+
+
+# 7.10 --------------------------------------------------------------------
+# Ctrl+swift+A - KAN RETTE KODEN FOR DIG
+
+# Object names
+day_One
+# T betyder true
+T <- FALSE
+# c er en funktion allerde, derfor må du ikke ændrer den.
+c <- 9
+
+# Spacing
+x[, 1]
+x[, 1]
+mean(x, na.rm = TRUE)
+mean(x, na.rm = TRUE)
+height <- (feet * 12) + inches
+df$z
+x <- 1:10
+
+# Indenting and brackets
+if (y < 0 && debug) {
+    message("y is negative")
+}
+
+
+
+# lib aktivere pakker
+
