870daeaecf1d7a8973d70bee2b39cad67cceee93
Christopher Rohde <stofferrohde1@gmail.com>
2021-06-16 13:37:35 +0200

    øvelse

--- R/exercises-wrangling.R
+++ R/exercises-wrangling.R
@@ -29,3 +29,22 @@ nhanes_small %>%
     select(phys_active_days, phys_active) %>%
     rename(days_phys_active = phys_active_days)
 
+
+
+
+################9.16
+
+# 1.
+nhanes_small %>%
+    summarise(mean_weight = mean(weight, na.rm = TRUE),
+              mean_age = mean(age))
+
+# 2.
+nhanes_small %>%
+    summarise(max_height = max(height, na.rm = TRUE),
+              min_height = min(height, na.rm = TRUE))
+
+# 3.
+nhanes_small %>%
+    summarise(median_age = median(age),
+                 median_phys_active_days = median(phys_active_days, na.rm = TRUE))
