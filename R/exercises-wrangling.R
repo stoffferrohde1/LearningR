786500970218f5e6212f360fb78bd0f6267d212f
Christopher Rohde <stofferrohde1@gmail.com>
2021-06-16 09:49:41 +0200

    Exercise nhanes

---
+++ R/exercises-wrangling.R
@@ -0,0 +1,15 @@
+# Load the packages
+source(here::here("R/package-loading.R"))
+
+# Check column names
+colnames(NHANES)
+
+# Look at contents
+str(NHANES)
+glimpse(NHANES)
+
+# See summary
+summary(NHANES)
+
+# Look over the dataset documentation
+?NHANES
