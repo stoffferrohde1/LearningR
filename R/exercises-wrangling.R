630d398a5845371617edb671008060e6adbb2b40
Christopher Rohde <stofferrohde1@gmail.com>
2021-06-16 11:13:33 +0200

    9.9

--- R/exercises-wrangling.R
+++ R/exercises-wrangling.R
@@ -13,3 +13,19 @@ summary(NHANES)
 
 # Look over the dataset documentation
 ?NHANES
+
+# 9.9
+
+nhanes_small %>%
+    select(tot_chol, bp_sys_ave, poverty)
+
+nhanes_small %>%
+    rename(diabetes_diagnosis_age = diabetes_age)
+
+nhanes_small %>%
+    select(bmi, contains("age"))
+
+nhanes_small %>%
+    select(phys_active_days, phys_active) %>%
+    rename(days_phys_active = phys_active_days)
+
